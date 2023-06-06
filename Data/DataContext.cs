c645aeaa68fd3ae9bbc774923cba34077fe9a967
EltonTahiri <eltontahiri4@gmail.com>
2023-06-06 11:52:04 +0200

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneHive.Models;
+
+
+namespace PhoneHive.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure any additional settings for entities
+        }
+    }
+}
